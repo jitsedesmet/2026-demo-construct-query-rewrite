696df7baed2f4a125da64713f7d9728bad2c1c8d
Jitse De Smet <jitse@reedonk8.be>
2026-05-27 17:49:41 +0200

    my examples

--- src/lib/exampleQueries.ts
+++ src/lib/exampleQueries.ts
@@ -12,95 +12,146 @@ interface QueryDescription {
 
 const identityMapping: MappingDescription = {
   label: 'Identity',
-  query: 'CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o }',
+  query: 'CONSTRUCT WHERE { ?s ?p ?o }',
 };
 
-export const exampleQueries: QueryDescription[] = [
-  {
-    name: 'Trustworthiness with RDF 1.2',
-    query: `VERSION "1.2"
-PREFIX geo:  <http://example.org/ontology/geo/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-SELECT ?territoryName
-       (COUNT(?claimant) AS ?claimantsCount)
-       (GROUP_CONCAT(?claimantName; separator=", ") AS ?claimants)
-WHERE {
-  << ?territory geo:partOf ?country >> prov:wasAttributedTo ?claimant .
-  ?claimant rdfs:label ?claimantName .
-  ?territory rdfs:label ?territoryName .
+function identityOfSource(source: string) {
+  return `CONSTRUCT { ?s ?p ?o }
+WHERE { SERVICE <${source}> {
+  ?s ?p ?o .
+} }`
 }
-GROUP BY ?territoryName
-HAVING (COUNT(?claimant) > 1)
-ORDER BY DESC(?claimantsCount)`,
-    mappings: [identityMapping],
-    sources: [
-      'https://raw.githubusercontent.com/rubensworks/rdf-12-examples/refs/heads/master/territories/data.ttl',
-      'https://fragments.dbpedia.org/2016-04/en',
-    ],
-  },
-  {
-    name: 'Exhaustive source selection (Bryan + Jitse co-authored with Ruben)',
-    query: `PREFIX schema: <http://schema.org/>
-PREFIX bibframe: <http://id.loc.gov/ontologies/bibframe/>
 
+const papersExample: QueryDescription = {
+  name: 'Exhaustive Source Selection (articles of Bryan, Jitse and Ruben)',
+  query: `PREFIX schema: <http://schema.org/>
+PREFIX bibframe: <http://id.loc.gov/ontologies/bibframe/>
 SELECT * WHERE {
   ?s schema:name ?name ;
-      a schema:ScholarlyArticle .
+       a schema:ScholarlyArticle .
 }`,
-    mappings: [
-      {
-        label: 'Bryan identity',
-        query: `CONSTRUCT { ?s ?p ?o }
-WHERE {
-  SERVICE <https://constraint-automaton.pp.ua/publication.ttl> {
-    ?s ?p ?o .
-  }
-}`,
-      },
-      {
-        label: 'Jitse identity',
-        query: `CONSTRUCT { ?s ?p ?o }
-WHERE {
-  SERVICE <https://jitsedesmet.be/profile#me> {
-    ?s ?p ?o .
-  }
-}`,
-      },
-      {
-        label: 'Ruben identity',
-        query: `CONSTRUCT { ?s ?p ?o }
-WHERE {
-  SERVICE <https://www.rubensworks.net/#me> {
-    ?s ?p ?o .
-  }
-}`,
-      },
-    ],
-    sources: [],
-  },
-  {
-    name: 'Spam filter (keep trusted claimants only)',
-    query: `VERSION "1.2"
-PREFIX dbr:  <http://dbpedia.org/resource/>
-PREFIX geo:  <http://example.org/ontology/geo/>
-PREFIX prov: <http://www.w3.org/ns/prov#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  mappings: [
+    {
+      label: 'Bryan',
+      query: identityOfSource('https://constraint-automaton.pp.ua/publication.ttl')
+    }, {
+      label: 'Jitse',
+      query: identityOfSource('https://jitsedesmet.be/profile#me')
+    }, {
+      label: 'Ruben T',
+      query: identityOfSource('https://www.rubensworks.net/#me')
+    }
+  ],
+  sources: [],
+};
 
-SELECT ?territoryName ?claimantName ?claimDate
-WHERE {
-  << dbr:Spratly_Islands geo:partOf ?country >> prov:wasAttributedTo ?claimant ;
-      geo:claimDate ?claimDate .
-  ?claimant rdfs:label ?claimantName .
-  dbr:Spratly_Islands rdfs:label ?territoryName .
-  FILTER (?claimant != dbr:Government_of_China)
+const wikidataExample: QueryDescription = {
+  name: 'Wikidata Reification',
+  query: `PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+SELECT * WHERE {
+  << wd:Q31 wdt:P31 ?country >> # pq:P582 ?end
+}`,
+  mappings: [
+    {
+      label: 'Triple Term',
+      query: `PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+CONSTRUCT {
+  ?t rdf:reifies <<( ?s ?wdt ?o )>>
+} WHERE {
+  ?s ?p ?t .
+  ?t ?ps ?o .
+  
+  ?wd wikibase:directClaim ?wdt ;
+          wikibase:claim ?p ;
+          wikibase:statementProperty ?ps . 
+}`,
+    },
+    identityMapping,
+  ],
+  sources: ['https://query.wikidata.org/sparql'],
 }
-ORDER BY ?claimDate`,
-    mappings: [identityMapping],
-    sources: [
-      'https://raw.githubusercontent.com/rubensworks/rdf-12-examples/refs/heads/master/territories/data.ttl',
-      'https://fragments.dbpedia.org/2016-04/en',
-    ],
-  },
+
+
+const uniprotExample: QueryDescription = {
+  name: 'uniprot Reification',
+  query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX up:  <http://purl.uniprot.org/core/>
+PREFIX uniprotkb: <http://purl.uniprot.org/uniprot/>
+SELECT * WHERE {
+  << uniprotkb:P01308 up:annotation ?annotation >> up:attribution ?attribution .
+  ?attribution up:evidence ?evidence ;   # ECO code (e.g. ECO_0000269 = experimental)
+               up:source   ?source .     # citation / PubMed entry
+}
+LIMIT 1`,
+  mappings: [
+    {
+      label: 'Triple Term',
+      query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+CONSTRUCT {
+  ?t rdf:reifies <<( ?s ?p ?o)>> 
+} WHERE {
+  ?t a rdf:Statement; rdf:subject ?s ;
+  rdf:predicate ?p ; rdf:object ?o .
+}`
+    },
+    identityMapping
+  ],
+  sources: [
+    'https://sparql.uniprot.org/sparql'
+  ],
+}
+
+const filterExample: QueryDescription = {
+  name: 'Paper Filter Verborgh',
+  query: `PREFIX schema: <http://schema.org/>
+
+SELECT * WHERE {
+  ?s schema:name ?o
+}`,
+  mappings: [
+    {
+      label: 'Canonical Mapping: (Jitse + RT) \ RV',
+      query: `PREFIX schema: <http://schema.org/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT {
+  ?article ?p ?o .
+} WHERE {
+  { {
+    SERVICE <https://jitsedesmet.be/profile/#me> {
+      ?article a schema:ScholarlyArticle;
+               schema:name ?nameL ;
+               ?p ?o .
+      BIND(STR(?nameL) AS ?name).
+    }
+  } UNION {
+    SERVICE <https://www.rubensworks.net/publications/>  {
+      ?article a schema:ScholarlyArticle ;
+               schema:name ?nameL ;
+               ?p ?o .
+      BIND(STR(?nameL) AS ?name).
+    }
+  }} MINUS {
+    SERVICE <https://ruben.verborgh.org/profile/#me> {
+      ?srv a schema:ScholarlyArticle ;
+           rdfs:label ?nameLL ;
+           ?p ?o .
+      BIND(STR(?nameLL) AS ?name).
+    }
+  } 
+}`
+    },
+  ],
+  sources: [],
+}
+
+export const exampleQueries: QueryDescription[] = [
+  wikidataExample,
+  papersExample,
+  uniprotExample,
+  filterExample
 ];
